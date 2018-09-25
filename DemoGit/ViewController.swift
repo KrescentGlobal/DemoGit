e313941c94bcfaaa94ab30a69fff31c230a2ebe1
KITLABS-M-003 <kitlabs-m-003@kitlabs-m-003s-mac-mini.local>
2018-09-25 16:46:47 +0530

    Initial Commit

---
+++ DemoGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoGit
+//
+//  Created by KITLABS-M-003 on 25/09/18.
+//  Copyright © 2018 KITLABS-M-003. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
